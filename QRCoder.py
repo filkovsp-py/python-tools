2b959d2da7247373daed4553713785a7c06a418e
P.F. <pavel.filkovskiy@gmail.com>
2026-02-12 16:01:24 +0000

    refactor

--- QRCoder.py
+++ QRCoder.py
@@ -1,9 +1,9 @@
-import io
 import qrcode
 import numpy as np
 import math
 
 from qrcode.image.svg import SvgImage
+from io import FileIO
 
 class QRCoder():
     
@@ -11,23 +11,27 @@ class QRCoder():
     
     @classmethod
     def encode(cls, file_name:str) -> None:
-        f = io.open(file_name, mode="r", encoding="utf-8")
-        file_text:list[str] = f.readlines()
-        line_count:int = len(file_text)
+        chunks:list = []
         
-        chunks:list = np.array_split(file_text, math.ceil(line_count / cls.line_max))
+        with open(file_name, mode="r", encoding="utf-8") as src:
+            file_text:list[str] = src.readlines()
+            line_count:int = len(file_text)
+        
+            chunks:list = np.array_split(file_text, math.ceil(line_count / cls.line_max))
 
         if len(chunks) == 0:
             raise Exception("Nothing to encode!")
 
         elif len(chunks) == 1:
             img:SvgImage = qrcode.make("".join(chunks[0]), image_factory=SvgImage)
-            img.save(f"{file_name}.svg")
+            with FileIO(f"{file_name}.svg", mode="w") as f:
+                img.save(f, kind="SVG")
 
         else:
             for i in range(len(chunks)):
                 img:SvgImage = qrcode.make("".join(chunks[i]), image_factory=SvgImage)
-                img.save(f"{file_name}.{i}.svg")
+                with FileIO(f"{file_name}.{i}.svg", mode="w") as f:
+                    img.save(f, kind="SVG")
 
     @classmethod
     def decode(cls, file_name:str) -> None:
